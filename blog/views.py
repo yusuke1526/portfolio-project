80b3cbd0137745376fc94d25e548e722bf176ae2
Yusuke Ueda <418810@m.mie-u.ac.jp>
2020-04-12 17:12:08 +0900

    Showing blogs order by newest to oldest

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allblogs(request):
-    blogs = Blog.objects
+    blogs = Blog.objects.order_by('-pub_date')
     return render(request, 'blog/allblogs.html', {'blogs': blogs})
 
 def detail(request, blog_id):
